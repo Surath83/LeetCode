3b4c2bb270c59d440af7ff480addf8f53613c9fe
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2026-02-15 22:36:57 +0530

    Time: 5 ms (11.95%) | Memory: 44.3 MB (12.54%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger aa = new BigInteger(a,2);
+        BigInteger bb = new BigInteger(b,2);
+        return aa.add(bb).toString(2);
+    }
+}
