313399f2e3b9f18ce6d0a58d75013a669329f28b
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2026-02-18 03:30:46 +0530

    Time: 12 ms (43.58%) | Memory: 47.6 MB (55.59%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,15 +1,36 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        // String[] str = new String[n];
+        // for(int i=0; i<n; i++){
+        //     pq.offer(new int[]{score[i],i});
+        // }
+        // int rank = 1;
+        // while(!pq.isEmpty()){
+        //     int[] arr = pq.poll();
+        //     int index = arr[1];
+        //     if(rank==1){
+        //         str[index] = "Gold Medal";
+        //     }else if(rank==2){
+        //         str[index] = "Silver Medal";
+        //     }else if(rank==3){
+        //         str[index] = "Bronze Medal";
+        //     }else{
+        //         str[index] = String.valueOf(rank);
+        //     }
+        //     rank++;
+        // }
+        // return str;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         String[] str = new String[n];
         for(int i=0; i<n; i++){
-            pq.offer(new int[]{score[i],i});
+            map.put(score[i],i);
         }
         int rank = 1;
-        while(!pq.isEmpty()){
-            int[] arr = pq.poll();
-            int index = arr[1];
+        for(int key : map.descendingKeySet()){
+            int index = map.get(key);
             if(rank==1){
                 str[index] = "Gold Medal";
             }else if(rank==2){
