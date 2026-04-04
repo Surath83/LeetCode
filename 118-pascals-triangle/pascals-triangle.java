11c78ecf755d2f6cea305bc9374a95d9b2854605
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2026-04-04 23:34:28 +0530

    Time: 1 ms (95.34%) | Memory: 43.5 MB (43.51%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void newRow(List<List<Integer>> list,int num){
+        if(num == 1){
+            list.add(new ArrayList<>(Arrays.asList(1)));
+            return;
+        }
+        if(num == 2){
+            list.add(new ArrayList<>(Arrays.asList(1,1)));
+            return;
+        }
+        List<Integer> prev = list.get(num - 2);
+        List<Integer> curr = new ArrayList<>();
+        curr.add(1);
+        for(int i=1; i<num-1; i++){
+            curr.add(prev.get(i-1)+prev.get(i));
+        }
+        curr.add(1);
+        list.add(curr);
+        return;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=1; i<=numRows; i++){
+            newRow(list,i);
+        }
+        return list;
+    }
+}
