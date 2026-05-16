2bd80bd3ac18e102b8c193e2255517d7c86d581c
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2026-05-16 10:19:25 +0530

    Time: 6 ms (16.25%) | Memory: 46.5 MB (80.93%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,58 @@
+import java.util.*;
+class Solution {
+    public boolean check(String[][] mat, int a, int b, int n) {
+        for (int i = 0; i < a; i++) {
+            if (mat[i][b].equals("Q")) {
+                return false;
+            }
+        }
+        for (int i = a - 1, j = b - 1; i >= 0 && j >= 0; i--, j--) {
+            if (mat[i][j].equals("Q")) {
+                return false;
+            }
+        }
+        for (int i = a - 1, j = b + 1; i >= 0 && j < n; i--, j++) {
+            if (mat[i][j].equals("Q")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<String> added(String[][] mat, int n) {
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder str = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                str.append(mat[i][j]);
+            }
+            temp.add(str.toString());
+        }
+        return temp;
+    }
+
+    public void solve(String[][] mat, int row, int n, List<List<String>> list){
+        if (row == n) {
+            list.add(added(mat, n));
+            return;
+        }
+        for (int j = 0; j < n; j++) {
+            if (check(mat, row, j, n)) {
+                mat[row][j] = "Q";
+                solve(mat, row + 1, n, list);
+                mat[row][j] = ".";
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> list = new ArrayList<>();
+        String[][] mat = new String[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = ".";
+            }
+        }
+        solve(mat, 0, n, list);
+        return list;
+    }
+}
